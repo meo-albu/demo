abff46664c975478080e0e8265cbfd31a034ac32
Marian Albu <albumeo1990@gmail.com>
2021-03-05 15:53:57 +0200

    added react-hook-form

--- src/ChangeUserDetails.js
+++ src/ChangeUserDetails.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import {Input, SIZE} from '@driven-crm/ui/Input'
 import {FormControl} from '@driven-crm/ui/FormControl'
+
 import {Button, SIZE as buttonSize} from '@driven-crm/ui/Button'
 import { useDispatch, useSelector } from 'react-redux'
 import { changeUser } from './store/actions/authActions'
@@ -11,6 +12,8 @@ const ChangeUserDetails = () => {
   const [disabled, setDisabled] = useState(true)
   const [name, setName] = useState('')
   const [position, setPosition] = useState('')
+
+
   const handleSubmit = e => {
     e.preventDefault()
 
@@ -33,18 +36,18 @@ const ChangeUserDetails = () => {
   }, [position, name])
 
   return (
-    <div className='mt-8'>
-      <div className='font-bold mb-3'>Change User Details</div>
-      <form onSubmit={handleSubmit}>
-        <FormControl label="Name" caption='User name'>
-          <Input size={SIZE.compact} name='name' value={name} onChange={e => setName(e.target.value)} />
-        </FormControl>
-        <FormControl label="Position" caption='Position name'>
-          <Input size={SIZE.compact} name='position' value={position} onChange={e => setPosition(e.target.value)} />
-        </FormControl>
-        <Button type='submit' disabled={disabled} size={buttonSize.compact}>Change Details</Button>
-      </form>
-    </div>
+      <div className='mt-8'>
+        <div className='font-bold mb-3'>Change User Details</div>
+        <form onSubmit={handleSubmit}>
+          <FormControl label="Name" caption='User name'>
+            <Input size={SIZE.compact} name='name' value={name} onChange={e => setName(e.target.value)} />
+          </FormControl>
+          <FormControl label="Position" caption='Position name'>
+            <Input size={SIZE.compact} name='position' value={position} onChange={e => setPosition(e.target.value)} />
+          </FormControl>
+          <Button type='submit' disabled={disabled} size={buttonSize.compact}>Change Details</Button>
+        </form>
+      </div>
   )
 }
 
--- src/auth/Login.js
+++ src/auth/Login.js
@@ -5,13 +5,19 @@ import {Button, SIZE as buttonSize} from '@driven-crm/ui/Button'
 import { useDispatch } from 'react-redux'
 import { login } from '../store/actions/authActions'
 
+import { useForm } from "react-hook-form";
+
 const Login = () => {
   const dispatch = useDispatch()
+
   const [disabled, setDisabled] = useState(true)
   const [name, setName] = useState('')
   const [password, setPassword] = useState('')
   const [position, setPosition] = useState('')
-  const handleSubmit = e => {
+
+  const { register, handleSubmit, errors, watch } = useForm();
+
+  const onSubmit = e => {
     e.preventDefault()
     if(name.length && password.length && position.length) {
       dispatch(login({
@@ -33,15 +39,48 @@ const Login = () => {
   return (
     <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-6 shadow-lg w-full md:w-1/2'>
       <div className='font-bold text-xl mb-3'>Login</div>
-      <form onSubmit={handleSubmit}>
-        <FormControl label="Name" caption='User name'>
-          <Input size={SIZE.compact} name='name' value={name} onChange={e => setName(e.target.value)} />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl label="Name" caption='User name'
+          error={errors.name?.type === 'required' ? 'Name is required' 
+          : errors.name?.type === 'minLength' ? 'Min 6 characters' : ''}
+        >
+          <Input 
+            size={SIZE.compact} 
+            name='name' 
+            value={name} 
+            onChange={e => setName(e.target.value)}
+            error={errors.name ? true : false}
+            ref={register({ required: true, minLength: 6 })}
+          />
         </FormControl>
-        <FormControl label="Password" caption='Password'>
-          <Input size={SIZE.compact} name='password' type='password' value={password} onChange={e => setPassword(e.target.value)} />
+        <FormControl 
+          label="Password" 
+          caption='Password'
+          error={errors.password && 'Password is required'}
+        >
+          <Input 
+          size={SIZE.compact} 
+          name='password' 
+          type='password' 
+          value={password} 
+          onChange={e => setPassword(e.target.value)} 
+          error={errors.password ? true : false}
+          ref={register({ required: true })} 
+        />
         </FormControl>
-        <FormControl label="Position" caption='Position name'>
-          <Input size={SIZE.compact} name='position' value={position} onChange={e => setPosition(e.target.value)} />
+        <FormControl 
+          label="Position" 
+          caption='Position name'
+          error={errors.position && 'position is required'}
+        >
+          <Input 
+            size={SIZE.compact} 
+            name='position' 
+            value={position} 
+            onChange={e => setPosition(e.target.value)} 
+            error={errors.position ? true : false}
+            ref={register({ required: true })} 
+          />
         </FormControl>
         <Button type='submit' disabled={disabled} size={buttonSize.compact}>Login</Button>
       </form>
